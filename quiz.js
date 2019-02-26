246c97420f4bebe4eb77104f6b064c465e0be298
Miloš Macura <milosmacura94@gmail.com>
2019-02-26 15:56:30 -0500

    initial commit

---
+++ quiz.js
@@ -0,0 +1,50 @@
+// Array of objects containing questions and answers
+const questions = [
+  {
+    question: "What is 10 + 10?",
+    options: ["8", "28", "30", "20"],
+    answer: "20"
+  },
+  {
+    question: "Who is the biggest cringelord in FDMM?",
+    options: ["Slavisa", "Macura", "Todor", "Majar"],
+    answer: "20"
+  }
+];
+
+// Counters for looping through questions and counting correct answers
+const questionNumber = 0;
+let correct = 0;
+
+// Load question and options
+function loadQuestion() {
+  document.getElementById("question").innerHTML =
+    questions[questionNumber].question;
+  const options = document.getElementById("options");
+  options.innerHTML = "";
+  for (option of questions[questionNumber].options) {
+    options.innerHTML += `<button class="option">${option}</button>`;
+  }
+}
+
+// Handle answer selection logic (correct / incorrect) --TO DO
+function selectAnswer() {
+  const buttons = document.getElementsByClassName("option");
+  const buttonsArray = Array.from(buttons);
+  buttonsArray.forEach(e => {
+    e.onclick = () => {
+      if (e.innerHTML == questions[questionNumber].answer) {
+        correct += 1;
+        alert(correct);
+      } else {
+        alert("False");
+      }
+    };
+  });
+}
+
+// Load functions into DOM interactively
+document.addEventListener("DOMContentLoaded", () => {
+  loadQuestion();
+  selectAnswer();
+});
